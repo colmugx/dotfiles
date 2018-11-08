9874aee390df05494638cde15f4d7a075ff0542a
ColMugX <colmugx@qq.com>
2018-11-08 14:05:26 +0800

    fix: fish path

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -1,5 +1,5 @@
-source .dotfiles/config/fish/paths.fish
-source .dotfiles/config/fish/mirrors.fish
+source ~/.dotfiles/config/fish/paths.fish
+source ~/.dotfiles/config/fish/mirrors.fish
 
 thefuck --alias | source
 set -U fish_prompt_pwd_dir_length 0
