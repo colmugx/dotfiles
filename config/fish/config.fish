d09afea251ef1cddc67cef68dfe6fa8604565ebe
colmugx <colmugx@qq.com>
2019-07-15 22:23:52 +0800

    add: conda

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -1,5 +1,9 @@
 source ~/.dotfiles/config/fish/paths.fish
 source ~/.dotfiles/config/fish/mirrors.fish
+source ~/.dotfiles/config/fish/aliases.fish
+
+#conda
+source ~/miniconda3/etc/fish/conf.d/conda.fish
 
 thefuck --alias | source
 set -U fish_prompt_pwd_dir_length 0
