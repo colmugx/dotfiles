82a0e14befc3e37162f688192f0c5401ca326a0a
colmugx <colmugx@qq.com>
2019-08-24 12:44:14 +0800

    update: mirrors.

--- config/fish/mirrors.fish
+++ config/fish/mirrors.fish
@@ -1,4 +1,4 @@
-set -gx ELECTRON_MIRROR https://npm.taobao.org/mirrors/electron
+set -gx ELECTRON_MIRROR https://npm.taobao.org/mirrors/electron/
 set -gx PUB_HOSTED_URL https://pub.flutter-io.cn
 set -gx FLUTTER_STORAGE_BASE_URL https://storage.flutter-io.cn
 set -gx RUSTUP_DIST_SERVER https://mirrors.ustc.edu.cn/rust-static
