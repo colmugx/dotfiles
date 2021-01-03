79e8aef05f8fd364472a8a43193b0879070d373d
colmugx <colmugx@qq.com>
2021-01-03 22:36:52 +0800

    remove thefuck

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -5,8 +5,6 @@ source ~/.dotfiles/config/fish/aliases.fish
 #conda
 source ~/miniconda3/etc/fish/conf.d/conda.fish
 
-thefuck --alias | source
-
 set -gx EDITOR 'nvim'
 set -gx GIT_EDITOR 'nvim'
 set -gx TERM 'screen-256color'
