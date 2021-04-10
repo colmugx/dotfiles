f0e58e13436f7bd6d6ee625f5e294f7fb9108785
colmugx <colmugx@qq.com>
2021-04-10 15:18:38 +0800

    update mirrors

--- config/fish/mirrors.fish
+++ config/fish/mirrors.fish
@@ -1,5 +1,5 @@
 set -gx ELECTRON_MIRROR https://npm.taobao.org/mirrors/electron/
-set -gx PUB_HOSTED_URL https://pub.flutter-io.cn
-set -gx FLUTTER_STORAGE_BASE_URL https://storage.flutter-io.cn
+set -gx PUB_HOSTED_URL https://mirrors.tuna.tsinghua.edu.cn/dart-pub
+set -gx FLUTTER_STORAGE_BASE_URL https://mirrors.tuna.tsinghua.edu.cn/flutter
 set -gx RUSTUP_DIST_SERVER https://mirrors.tuna.tsinghua.edu.cn/rustup
 set -gx RUSTUP_UPDATE_ROOT https://mirrors.ustc.edu.cn/rust-static/rustup
