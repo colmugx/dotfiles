80cbcc952503b0a200de8e7d848899faf4869c26
colmugx <colmugx@qq.com>
2022-01-18 10:15:57 +0800

    update filetree

--- config/nvim/lua/modules/filetree.lua
+++ config/nvim/lua/modules/filetree.lua
@@ -30,10 +30,19 @@ function Module.Setup(use)
 
     use {
         "kyazdani42/nvim-tree.lua",
-        requires = {'kyazdani42/nvim-web-devicons'
-        },
+        requires = {'kyazdani42/nvim-web-devicons'},
         config = function()
-            require("nvim-tree").setup {}
+            local tree_cb = require'nvim-tree.config'.nvim_tree_callback
+            require("nvim-tree").setup {
+                view = {
+                    mappings = {
+                        list = {{
+                            key = "i",
+                            cb = tree_cb("split")
+                        }}
+                    }
+                }
+            }
 
             vim.api.nvim_set_keymap('n', '<Leader>ft', '<cmd>NvimTreeToggle<cr>', {
                 noremap = true
