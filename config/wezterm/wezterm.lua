fc6938f7b7c4562ef1ecc476df6efec258d9e37f
colmugx <colmugx@qq.com>
2022-04-24 23:48:48 +0800

    add wezterm

---
+++ config/wezterm/wezterm.lua
@@ -0,0 +1,12 @@
+local wezterm = require "wezterm"
+
+return {
+  color_scheme = "OneHalfDark",
+  window_decorations = "RESIZE",
+  hide_tab_bar_if_only_one_tab = true,
+  default_cursor_style = "SteadyBar",
+
+  font = wezterm.font("JetBrains Mono"),
+  font_size = 12.0,
+}
+
