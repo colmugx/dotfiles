c55445659c88224fc4f43d056e69f7737cda1085
colmugx <colmugx@qq.com>
2022-12-14 15:39:05 +0800

    update wezterm

--- config/wezterm/wezterm.lua
+++ config/wezterm/wezterm.lua
@@ -2,11 +2,16 @@ local wezterm = require "wezterm"
 
 return {
   color_scheme = "OneHalfDark",
-  window_decorations = "RESIZE",
+  -- window_decorations = "RESIZE",
   hide_tab_bar_if_only_one_tab = true,
-  default_cursor_style = "SteadyBar",
+  default_cursor_style = "SteadyBlock",
 
   font = wezterm.font("JetBrains Mono"),
-  font_size = 12.0,
+  font_size = 13.0,
+
+  window_padding = {
+    top = 0,
+    bottom = 0,
+  }
 }
 
