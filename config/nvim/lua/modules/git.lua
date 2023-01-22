cf44ea6882b47b2fdbf416eeedf8f6d29e8fc640
colmugx <colmugx@qq.com>
2023-01-22 12:16:26 +0800

    update nvim config

--- config/nvim/lua/modules/git.lua
+++ config/nvim/lua/modules/git.lua
@@ -2,20 +2,34 @@ local Module = {}
 
 function Module.Setup(use)
 
-    use {{
+    use {
         "lewis6991/gitsigns.nvim",
         requires = {"nvim-lua/plenary.nvim"},
         config = function()
-            require("gitsigns").setup()
+            require("gitsigns").setup {
+              current_line_blame = true
+            }
         end
-    }, {
+    }
+
+    use {
         "rhysd/git-messenger.vim",
         config = function()
             vim.api.nvim_set_keymap("n", "<Leader>gm", "<CMD>GitMessenger<CR>", {
                 noremap = true
             })
         end
-    }}
+    }
+
+    use {
+        'tanvirtin/vgit.nvim',
+        requires = {
+          'nvim-lua/plenary.nvim'
+        },
+        config = function()
+            require('vgit').setup()
+        end
+    }
 
 end
 
